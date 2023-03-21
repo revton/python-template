9e8bac0ba8f5c8e9c108dbad1ff8af7d18cb506f
Revton Braga <revtonbr@gmail.com>
2023-03-20 21:29:24 -0300

    feat: teste unitario e capturando stdout

--- tests/test_unitario.py
+++ tests/test_unitario.py
@@ -1,2 +1,9 @@
-def test_1_igual_1():
-    assert 1 == 1
+from app import soma
+
+def test_1_mais_1_igual_2():
+    assert soma(1,1) == 2
+
+
+def test_print(capture_stdout):
+    print('Hello World')
+    assert capture_stdout["stdout"] == 'Hello World\n'
